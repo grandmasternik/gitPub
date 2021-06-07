306988fe132daf26c9f734a290db0dc896c27feb
Nikki Mallory <nikkimallory13@gmail.com>
2021-06-06 21:08:53 -0500

    drinks database added and connected

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const drinks = require(`./models/drinks`);
 
 // ========Routes===========//
 app.get("/", (req, res)=>{
     res.send("Welcome to the Gitpub App");
+});
+
+app.get("/drinks", (req, res)=>{
+    res.send(drinks)
 });
 
 // ========Web Server========//
